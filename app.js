753cdfb55738ce2676ae218251deeb86da494427
Morgan West <mewwes@gmail.com>
2022-12-14 23:33:45 -0700

    First round Memory Game
    Please review and figure out why the cards are just showing up blank with only the score populating

---
+++ app.js
@@ -0,0 +1,110 @@
+document.addEventListener('DOMContentLoaded', () => {
+//card options
+    const cardArray = [
+        {
+            name: 'Lizard',
+            img: 'images/lizard-memory-game-sq.jpp'
+        },
+        {
+            name: 'Lizard',
+            img: 'images/lizard-memory-game-sq.jpg'
+        },
+        {
+            name: 'Panda',
+            img: 'panda-memory-game-sq.jpg'
+        },
+        {
+            name: 'Panda',
+            img: 'panda-memory-game-sq.jpg'
+        },
+        {
+            name: 'Dolphin',
+            img: 'dolphin-memory-card-game-sq.jpg'
+        },
+        {
+            name: 'Dolphin',
+            img: 'dolphin-memory-card-game-sq.jpg'
+        },
+        {
+            name:'Rhino',
+            img: 'rhino-memory-game-sq.jpg'
+        },
+        {
+            name:'Rhino',
+            img: 'rhino-memory-game-sq.jpg'
+        },
+        {
+            name:'Tiger',
+            img:'tiger-memory-game-sq.jpg'
+        },
+        {
+            name:'Tiger',
+            img:'tiger-memory-game-sq.jpg'
+        },
+        {
+            name:'Baboon',
+            img:'baboon-memory-game.jpg'
+        },
+        {
+            
+            name:'Baboon',
+            img:'baboon-memory-game-sq.jpg'
+        }
+
+    ]
+
+    cardArray.sort(() => 0.5 - Math.random())
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenID =[]
+    var cardsWon = []
+
+    //create your board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card= document.createElement('img')
+            card.setAttribute('src', 'images/front-card-memory-game-sq.jpg')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipcard)
+            grid.appendChild(card)
+        }
+    }
+
+    //check for matches
+    function checkForMatch() {
+        var cards = document. querySelectorAll('img')
+        const optionOneID = cardsChosenID[0]
+        const optionTwoID = cardsChosenID[1]
+        if (cardsChosen[0]=== cardsChosen[1]) {
+            alert ('You found a match')
+            cards[optionOneID].setAttribute('src', 'images/white-memory-game-sq-jpg')
+            cards[optionTwoID].setAttribute('src', 'images/white-memory-game-sq-jpg')
+            cardsWon.push(cards)
+        } else {
+            cards[optionOneID].setAttribute('src', 'images/front-card-memory-game-sq.jpg')
+            cards[optionTwoID].setAttribute('src', 'images/front-card-memory-game-sq.jpg')
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenID = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+        }
+    }
+
+
+    //flip your card
+    function flipCard() {
+        let cardID = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardID].name)
+        cardsChosenID.push(cardID)
+        this.setAttribute('src', cardArray[cardID].img)
+        if (cardsChosen.length === 2) {
+           setTimeout(checkForMatch, 500) 
+        }
+    }
+
+    createBoard()
+})
